3e0acb6f6545556c8b4ddfb054eb49674e20cea3
Medhavi Sahgal <112403556+medhavisahgal@users.noreply.github.com>
2023-07-04 00:59:28 +0530

    Create prime factorization.cpp

---
+++ Number Theory/prime factorization.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> prime_factor;
+  //if a number is a composite number
+    for (int i = 2; i <= sqrt(n) + 1; i++) {
+        while (n % i == 0) {
+            prime_factor.push_back(i);
+            n /= i;
+        }
+    }
+  //if a number is a prime number
+    if (n > 1) {
+        prime_factor.push_back(n);
+    }
+    for (int i = 0; i < prime_factor.size(); i++) {
+        cout << prime_factor[i] << " ";
+    }
+    return 0;
+}
