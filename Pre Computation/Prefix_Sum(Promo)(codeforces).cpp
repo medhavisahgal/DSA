4d4eab16006f4c11af21086eedab6c4cceecba7d
Medhavi Sahgal <112403556+medhavisahgal@users.noreply.github.com>
2023-08-02 17:58:19 +0530

    Update Prefix_Sum(Promo)(codeforces).cpp

--- Pre Computation/Prefix_Sum(Promo)(codeforces).cpp
+++ Pre Computation/Prefix_Sum(Promo)(codeforces).cpp
@@ -10,15 +10,25 @@ int main()
         cin>>p[i];
     }
     sort(p,p+n);
+    //p[n]=[1,2,3,4,5]
     for(int i=0; i<n; i++)
     {
         pf[i+1]=pf[i]+p[i];
     }
+    //pf[1]=pf[0]-->(0)+p[0]-->1
+    //pf[1]=1
+    //pf[2]=pf[1]-->1+p[1]-->2
+    //pf[2]=3
+    //pf[3]=pf[2]-->3+p[2]-->3
+    //pf[3]=6
+    //so on pf[n+1]=[0,1,3,6,10,15] 
     while(q--)
     {
         int x,y;
         cin>>x>>y;
         cout<<pf[n-(x-y)]-pf[n-x]<<endl;
+        //pf[5-(3-2)]-pf[5-3]
+        //pf[4]-pf[2]=6-3-->3
     }
     return 0;
 }
