7791d5c9925162ad1b8440fc383c491112d085c1
Medhavi Sahgal <112403556+medhavisahgal@users.noreply.github.com>
2023-10-23 21:14:21 +0530

    Update Trick1.cpp

--- Bitwise/Trick1.cpp
+++ Bitwise/Trick1.cpp
@@ -1,1 +1,23 @@
-
+//check if the number is even or odd
+//They are much faster then normal operations
+//eg (3)=(0011)&(0001)==(0001)=True
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  cin>>a;
+  // if(a%2==0){
+  //   court<<"Even"<<endl;
+  // }
+  // else{
+  //   cout<<"odd"<<endl;
+  // }
+  //-----Instead Do this ---
+  if(a&1){
+    cout<<"ODD"<<endl;
+  }
+  else{
+    cout<<"EVEN"<<endl;
+  }
+  return 0;
+}
