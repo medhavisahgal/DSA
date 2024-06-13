871e354556c7349911eb3facf1619ec51353191b
Medhavi Sahgal <112403556+medhavisahgal@users.noreply.github.com>
2024-06-13 12:03:32 +0530

    Create Boyer-Moore Majority Voting Algorithm.cpp

---
+++ Named Algorithms/Boyer-Moore Majority Voting Algorithm.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+      int cnt=0;
+      int ele;
+      for(auto x:nums){
+        if(cnt==0){
+            cnt=1;
+            ele=x;
+        }
+        else if(x==ele){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+      }
+      return ele;
+    }
+};
