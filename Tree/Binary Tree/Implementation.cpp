a92cf1f1329cc977e5950b4624551f41c18b2862
medhavisahgal <112403556+medhavisahgal@users.noreply.github.com>
2024-07-07 16:55:48 +0530

    added implementation

---
+++ Tree/Binary Tree/Implementation.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data1)
+    {
+        data = data1;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+// array representation of binary tree
+
+
