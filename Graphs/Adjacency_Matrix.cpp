fd70deb93620a32a1da53480231b8aff8ed88455
Medhavi <18medhavi3418@gmail.com>
2024-09-03 21:38:54 +0530

    new

---
+++ Graphs/Adjacency_Matrix.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>adjm(n,vector<int>(n,0));
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adjm[u][v]=1;
+        adjm[v][u]=1;//for undirected graph
+    }
+    return 0;
+}
