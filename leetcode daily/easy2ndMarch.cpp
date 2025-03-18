4f3a19758a7ccce3732e422450013c3794d31a2e
medhavisahgal <18medhavi3418@gmail.com>
2025-03-18 14:32:27 +0530

    modified

--- leetcode daily/easy2ndMarch.cpp
+++ leetcode daily/easy2ndMarch.cpp
@@ -1,67 +1,94 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
 class Solution
 {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>> &nums1, vector<vector<int>> &nums2)
     {
-        vector<vector<int>> nums3;
-
+        // vector<vector<int>> nums3;
         // Merge common elements from nums1 and nums2
-        for (int i = 0; i < nums1.size(); i++)
-        {
-            for (int j = 0; j < nums2.size(); j++)
-            {
-                if (nums1[i][0] == nums2[j][0])
-                {
-                    int pushb = nums1[i][1] + nums2[j][1];
-                    nums3.push_back({nums1[i][0], pushb});
-                    break;
-                }
-            }
-        }
+        // for (int i = 0; i < nums1.size(); i++)
+        // {
+        //     for (int j = 0; j < nums2.size(); j++)
+        //     {
+        //         if (nums1[i][0] == nums2[j][0])
+        //         {
+        //             int pushb = nums1[i][1] + nums2[j][1];
+        //             nums3.push_back({nums1[i][0], pushb});
+        //             break;
+        //         }
+        //     }
+        // }
 
         // Insert unique elements from nums1
-        for (int i = 0; i < nums1.size(); i++)
-        {
-            bool found = false;
-            for (int j = 0; j < nums3.size(); j++)
-            {
-                if (nums1[i][0] == nums3[j][0])
-                {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found)
-            {
-                nums3.push_back(nums1[i]);
-            }
-        }
+        // for (int i = 0; i < nums1.size(); i++)
+        // {
+        //     bool found = false;
+        //     for (int j = 0; j < nums3.size(); j++)
+        //     {
+        //         if (nums1[i][0] == nums3[j][0])
+        //         {
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if (!found)
+        //     {
+        //         nums3.push_back(nums1[i]);
+        //     }
+        // }
 
         // Insert unique elements from nums2
-        for (int i = 0; i < nums2.size(); i++)
+        // for (int i = 0; i < nums2.size(); i++)
+        // {
+        //     bool found = false;
+        //     for (int j = 0; j < nums3.size(); j++)
+        //     {
+        //         if (nums2[i][0] == nums3[j][0])
+        //         {
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if (!found)
+        //     {
+        //         nums3.push_back(nums2[i]);
+        //     }
+        // }
+        // sort(nums3.begin(), nums3.end());
+        // optimized approach - two pointer approach
+        vector<vector<int>> nums3;
+        int i = 0, j = 0;
+        while (i < nums1.size() && j < nums2.size())
         {
-            bool found = false;
-            for (int j = 0; j < nums3.size(); j++)
+            if (nums1[i][0] == nums2[j][0])
             {
-                if (nums2[i][0] == nums3[j][0])
-                {
-                    found = true;
-                    break;
-                }
+                nums3.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
             }
-            if (!found)
+            else if (nums1[i][0] < nums2[j][0])
             {
-                nums3.push_back(nums2[i]);
+                nums3.push_back({nums1[i][0], nums1[i][1]});
+                i++;
+            }
+            else
+            {
+                nums3.push_back({nums2[j][0], nums2[j][1]});
+                j++;
             }
         }
-
-        // Sort by first element
-        sort(nums3.begin(), nums3.end());
-
+        while (i < nums1.size())
+        {
+            nums3.push_back(nums1[i]);
+            i++;
+        }
+        while (j < nums2.size())
+        {
+            nums3.push_back(nums2[j]);
+            j++;
+        }
         return nums3;
     }
 };
